0bbc6e3475d59dc7df8f6eb800fda4c3cd36fa6e
Madalina <bumbamadalina@yahoo.com>
2018-10-10 10:46:37 +0300

    MergeLogin

---
+++ Assets/scripts/LoginScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginScript : MonoBehaviour {
+
+    public GameObject username;
+    public GameObject login;
+
+    public Button btnlogin;
+
+    private string Username;
+
+
+    // Use this for initialization
+    void Start () {
+        Screen.orientation = ScreenOrientation.Portrait;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //set data to String from input fields
+        Username = username.GetComponent<InputField>().text;
+
+        btnlogin = login.GetComponent<Button>();
+        btnlogin.onClick.AddListener(validateLogin);
+    }
+    private void validateLogin()
+    {
+        if (Username != "")
+        {
+            print("Login Success");
+            SceneManager.LoadScene("LoginScene");
+        }
+
+    }
+}
