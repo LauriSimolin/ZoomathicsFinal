d52840d3b64045bd94716a256f57b3461e882dd3
Madalina <bumbamadalina@yahoo.com>
2018-10-16 19:29:49 +0300

    Revert "Revert "redbuttonsandbackgroundmap""
    This reverts commit 79324882912c35b5259f53ef15daaafe75a84ab0.

---
+++ Assets/Sprites/SFXPlaying.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlaying : MonoBehaviour {
+
+    public AudioSource Spawnpoint;
+
+    public void PlaySpawnpoint()
+    {
+        Spawnpoint.Play();
+    }
+}
