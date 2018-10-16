4a941c7f0bfe234b4aa2bb0d1e16543ff5404236
marinela1800 <43773161+marinela1800@users.noreply.github.com>
2018-10-16 19:16:29 +0300

    SaveScript

---
+++ Assets/scripts/GameData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+
+    public int GameInteger { get; set; }
+    public string GameString { get; set; }
+
+    [SerializeField]
+    private Text textInteger;
+    [SerializeField]
+    private Text textString;
+
+    public void GenerateNewData()
+    {
+        GameInteger = Random.Range(1, 1000);
+        GameString = System.Convert.ToBase64String(System.BitConverter.GetBytes(GameInteger));
+        ShowData();
+    }
+
+    public void ShowData()
+    {
+        textInteger.text = GameInteger.ToString();
+        textString.text = GameString;
+    }
+} 
